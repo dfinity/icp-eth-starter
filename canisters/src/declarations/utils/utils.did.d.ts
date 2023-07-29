3289e1c12241c56092948e7eb2cea6ed7695d51a
rvanasa <ryan.vandersmith@dfinity.org>
2023-07-29 10:18:31 -0600

    Remove 'canisters/src/declarations' directory

--- canisters/src/declarations/utils/utils.did.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface _SERVICE {
-  'verify_ecdsa' : ActorMethod<[string, string, string], boolean>,
-}
