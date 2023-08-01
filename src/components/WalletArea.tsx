3f471a68b5530248f4367bbde1e0a369fb1dbc25
rvanasa <ryan.vandersmith@dfinity.org>
2023-08-01 11:26:43 -0600

    Progress

--- src/components/WalletArea.tsx
+++ src/components/WalletArea.tsx
@@ -1,12 +1,86 @@
+import { ethers } from 'ethers';
 import { useMetaMask } from 'metamask-react';
+import { useEffect, useMemo } from 'react';
 import { FaEthereum } from 'react-icons/fa';
+import { styled } from 'styled-components/macro';
 import tw from 'twin.macro';
+import { useSessionStorage } from '../hooks/utils/useLocalStorage';
+import { erc721Abi } from '../utils/abi/erc721';
+
+const FormContainer = styled.form`
+  ${tw`space-y-4`}
+
+  label {
+    ${tw`flex flex-col gap-2 w-full text-xl font-semibold`}
+    > * {
+      ${tw`text-lg font-normal`}
+    }
+  }
+
+  input[type='text'],
+  input[type='number'],
+  textarea {
+    ${tw`w-full border-2 p-2 rounded-lg`}
+  }
+`;
 
 export const WalletAreaButton = tw.div`flex items-center gap-2 px-4 py-2 border-2 text-lg rounded-full cursor-pointer select-none bg-[#fff8] hover:bg-gray-100`;
 
 export default function WalletArea() {
   // const user = useIdentity();
-  const { status, connect, account, chainId /* , ethereum */ } = useMetaMask();
+  const { status, connect, account, chainId, ethereum } = useMetaMask();
+  const [nftUrl, setNftUrl] = useSessionStorage('ic-eth.nft-url', '');
+  // const [nftImage, setNftImage] = useState<string>();
+
+  const parseNft = (nftUrl: string) => {
+    const groups =
+      /https:\/\/(testnets\.)?opensea.io\/assets\/(\w+)\/0x(\w+)\/(\d+)/.exec(
+        nftUrl,
+      );
+    if (!groups) {
+      return;
+    }
+    const [, , network, address, tokenId] = groups;
+    return {
+      network,
+      address,
+      tokenId: Number(tokenId),
+    };
+  };
+
+  const nft = useMemo(() => parseNft(nftUrl), [nftUrl]);
+
+  console.log(nft); ////
+
+  useEffect(() => {
+    if (nft) {
+      // handlePromise(
+      //   ethereum
+      //     .request('eth_call', {
+      //       from: ethereum.selectedAddress,
+      //       to: '',
+      //       // value:
+      //     })
+      //     .then(() => {
+      //       // let ethersProvider = new ethers.providers.Web3Provider(
+      //       //   ethereum,
+      //       //   'any',
+      //       // );
+      //       // let provider = ethers.getDefaultProvider(nft.network);
+
+      //     }),
+      //   undefined,
+      //   'Error while requesting',
+      // );
+
+      let contract = new ethers.Contract(
+        '0x7236dA2B814f496ef31b73b461Fbd3309EFe4dcE',
+        erc721Abi,
+        // provider,
+      );
+      console.log(contract); ///
+    }
+  }, [ethereum, nft]);
 
   const getMetaMaskButton = () => {
     if (status === 'initializing') {
@@ -34,8 +108,20 @@ export default function WalletArea() {
   };
 
   return (
-    <div>
+    <>
       <div tw="mx-auto">{getMetaMaskButton()}</div>
-    </div>
+      <hr tw="my-5" />
+      <FormContainer>
+        <label>
+          OpenSea NFT
+          <input
+            type="text"
+            placeholder="Paste URL here"
+            value={nftUrl}
+            onChange={(e) => setNftUrl(e.target.value)}
+          />
+        </label>
+      </FormContainer>
+    </>
   );
 }
--- src/components/pages/ProfilePage.tsx
+++ src/components/pages/ProfilePage.tsx
@@ -4,64 +4,40 @@ import { handlePromise } from '../../utils/handlers';
 import LoginArea, { LoginAreaButton } from '../LoginArea';
 import WalletArea from '../WalletArea';
 import Page from '../utils/Page';
-import tw from 'twin.macro';
-import styled from 'styled-components/macro';
-
-const ProfileContainer = styled.form`
-  /* ${tw`space-y-4`}
-
-  label {
-    ${tw`flex flex-col gap-2 w-full text-xl font-semibold`}
-    > * {
-      ${tw`text-lg font-normal`}
-    }
-  }
-
-  input[type='text'],
-  input[type='number'],
-  textarea {
-    ${tw`w-full border-2 p-2 rounded-lg`}
-  } */
-`;
+import 'twin.macro';
 
 export default function ProfilePage() {
   const user = useIdentity();
 
   return (
     <Page>
-      <ProfileContainer>
-        {user ? (
-          <>
-            <div tw="flex flex-col md:flex-row items-center">
-              <div tw="flex-1 pb-4 text-xl text-gray-600">
-                Signed in with principal:
-                <span tw="block text-sm font-bold mt-1">
-                  {user.client.getIdentity().getPrincipal().toString()}
-                </span>
-              </div>
-              <div tw="flex flex-col items-center mt-3 sm:mt-0">
-                <LoginAreaButton
-                  tw="flex gap-1 items-center text-base px-4"
-                  onClick={() =>
-                    handlePromise(
-                      logout(),
-                      undefined,
-                      'Error while signing out!',
-                    )
-                  }
-                >
-                  <FaSignOutAlt />
-                  <span tw="font-semibold select-none ml-1">Sign out</span>
-                </LoginAreaButton>
-              </div>
+      {user ? (
+        <>
+          <div tw="flex flex-col md:flex-row items-center">
+            <div tw="flex-1 pb-4 text-xl text-gray-600">
+              Signed in with principal:
+              <span tw="block text-sm font-bold mt-1">
+                {user.client.getIdentity().getPrincipal().toString()}
+              </span>
             </div>
-            <hr tw="my-5" />
-            <WalletArea />
-          </>
-        ) : (
-          <LoginArea />
-        )}
-      </ProfileContainer>
+            <div tw="flex flex-col items-center mt-3 sm:mt-0">
+              <LoginAreaButton
+                tw="flex gap-1 items-center text-base px-4"
+                onClick={() =>
+                  handlePromise(logout(), undefined, 'Error while signing out!')
+                }
+              >
+                <FaSignOutAlt />
+                <span tw="font-semibold select-none ml-1">Sign out</span>
+              </LoginAreaButton>
+            </div>
+          </div>
+          <hr tw="my-5" />
+          <WalletArea />
+        </>
+      ) : (
+        <LoginArea />
+      )}
     </Page>
   );
 }
