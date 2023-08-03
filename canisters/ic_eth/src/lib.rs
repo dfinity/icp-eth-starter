a83ea3fe8515bbcc5bf5a044bd22fd4209ce3372
matthewhammer <matthew@dfinity.org>
2023-08-03 11:20:08 -0600

    ERC 1155 balanceOf

--- canisters/ic_eth/src/lib.rs
+++ canisters/ic_eth/src/lib.rs
@@ -155,3 +155,135 @@ fn preprocess_address<'a>(address: &'a str) -> &'a str {
         address
     }
 }
+
+#[ic_cdk_macros::update]
+#[candid_method]
+pub async fn erc1155_balance_of(
+    network: String,
+    nft_contract_address: String,
+    owner_address: String,
+    token_id: usize,
+) -> usize {
+    // to do -- use BigUint ?
+    // Remove leading `0x` when found
+    let network = preprocess_address(&network);
+    let nft_contract_address = preprocess_address(&nft_contract_address);
+    let owner_address = preprocess_address(&owner_address);
+    let owner_address = ethers_core::types::H160::from_slice(owner_address.as_bytes());
+
+    let max_response_bytes = 2048;
+    let service_url = match network {
+        "mainnet" => "https://cloudflare-eth.com",
+        "sepolia" => "https://rpc.sepolia.org",
+        _ => panic!("Unknown network: {}", network),
+    }
+    .to_string();
+
+    #[allow(deprecated)]
+    let f = abi::Function {
+        name: "balanceOf".to_string(),
+        inputs: vec![
+            abi::Param {
+                name: "account".to_string(),
+                kind: abi::ParamType::Address,
+                internal_type: None,
+            },
+            abi::Param {
+                name: "id".to_string(),
+                kind: abi::ParamType::Uint(256),
+                internal_type: None,
+            },
+        ],
+        outputs: vec![abi::Param {
+            name: "".to_string(),
+            kind: abi::ParamType::Uint(256),
+            internal_type: None,
+        }],
+        constant: None,
+        state_mutability: abi::StateMutability::View,
+    };
+
+    let data = hex::encode(
+        f.encode_input(&[
+            abi::Token::Address(owner_address.into()),
+            abi::Token::Uint(token_id.into()),
+        ])
+        .expect("encode_input"),
+    );
+
+    #[derive(Serialize)]
+    struct JsonRpcRequest {
+        id: usize,
+        jsonrpc: String,
+        method: String,
+        params: (EthCallParams, String),
+    }
+    #[derive(Serialize)]
+    struct EthCallParams {
+        to: String,
+        data: String,
+    }
+
+    let json_rpc_payload = serde_json::to_string(&JsonRpcRequest {
+        id: 1, // TODO: possibly increment id for each call?
+        jsonrpc: "2.0".to_string(),
+        method: "eth_call".to_string(),
+        params: (
+            EthCallParams {
+                to: format!("0x{nft_contract_address}").to_string(),
+                data: format!("0x{data}").to_string(),
+            },
+            "latest".to_string(),
+        ),
+    })
+    .expect("Error while encoding JSON-RPC request");
+
+    let parsed_url = url::Url::parse(&service_url).expect("Service URL parse error");
+    let host = parsed_url
+        .host_str()
+        .expect("Invalid service URL host")
+        .to_string();
+
+    let request_headers = vec![
+        HttpHeader {
+            name: "Content-Type".to_string(),
+            value: "application/json".to_string(),
+        },
+        HttpHeader {
+            name: "Host".to_string(),
+            value: host.to_string(),
+        },
+    ];
+    let request = CanisterHttpRequestArgument {
+        url: service_url,
+        max_response_bytes: Some(max_response_bytes),
+        method: HttpMethod::POST,
+        headers: request_headers,
+        body: Some(json_rpc_payload.as_bytes().to_vec()),
+        transform: None,
+    };
+    let result = match make_http_request(request, 5000000).await {
+        Ok((r,)) => r,
+        Err((r, m)) => panic!("{:?} {:?}", r, m),
+    };
+
+    #[derive(Deserialize)]
+    struct JsonRpcResult {
+        result: Option<usize>,
+        error: Option<JsonRpcError>,
+    }
+    #[derive(Deserialize)]
+    struct JsonRpcError {
+        code: isize,
+        message: String,
+        // data: String,
+    }
+    let json: JsonRpcResult =
+        serde_json::from_str(std::str::from_utf8(&result.body).expect("utf8"))
+            .expect("JSON was not well-formatted");
+    if let Some(err) = json.error {
+        panic!("JSON-RPC error code {}: {}", err.code, err.message);
+    }
+    let result = json.result.expect("Unexpected JSON response");
+    result
+}
