bd263dd84be9381655a9394d5a319bc7024dbd13
rvanasa <ryan.vandersmith@dfinity.org>
2023-08-05 11:29:30 -0600

    Set up 'NftCard' component

---
+++ src/components/NftCard.tsx
@@ -0,0 +1,36 @@
+import { Nft } from '../declarations/backend/backend.did';
+import { useNftMetadata } from '../services/nftService';
+import Loading from './utils/Loading';
+import 'twin.macro';
+
+export default function NftCard({ nft }: { nft: Nft }) {
+  const [metadata] = useNftMetadata(nft.network, nft.contract, nft.tokenId);
+
+  return (
+    <div tw="p-5 bg-white rounded-lg space-y-3 drop-shadow-2xl max-w-[200px]">
+      {metadata ? (
+        <>
+          {!!metadata.title && (
+            <div tw="text-2xl sm:text-xl font-bold">{metadata.title}</div>
+          )}
+          {!!metadata.media.length && (
+            <img
+              tw="w-full rounded-xl"
+              alt="NFT preview"
+              src={metadata.media[0].gateway}
+            />
+          )}
+          {/* {!!metadata.description && (
+            <div tw="sm:text-xl">{metadata.description}</div>
+          )} */}
+        </>
+      ) : metadata === undefined ? (
+        <div tw="flex w-full items-center">
+          <Loading />
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
