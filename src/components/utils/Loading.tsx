ba355a60f216fc5d31d064f2998a6fc7d86e4315
Ryan Vandersmith <ryan.vandersmith@dfinity.org>
2023-08-23 22:43:44 -0600

    Prune unused files (#45)
    * Remove unused files
    
    * Remove unused CSS files
    
    * Remove more TypeScript files
    
    * Remove unused npm dependencies

--- src/components/utils/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { FaEllipsisH } from 'react-icons/fa';
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center my-10 opacity-50 text-5xl text-white">
-      <FaEllipsisH />
-    </div>
-  );
-}
--- src/components/utils/Page.tsx
+++ src/components/utils/Page.tsx
@@ -1,3 +1,4 @@
 import tw from 'twin.macro';
 
-export default tw.div`p-5 sm:(px-10 py-8) bg-gray-100 rounded-xl`;
+const Page = tw.div`p-5 sm:(px-10 py-8) bg-gray-100 rounded-xl`;
+export default Page;
